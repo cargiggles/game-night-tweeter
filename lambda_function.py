d366a0b9d7dc8795bdc5a144f51b1eaf8e83e813
Andrew Cargill <20098785+cargiggles@users.noreply.github.com>
2018-05-26 14:41:53 -0700

    Read Write Cleanup

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,6 @@
 # Andrew Cargill
 # Game Night Tweeter
-# 2018-05-24 - v1.2
+# 2018-05-26 - v1.3
 
 import boto3
 import os
@@ -18,7 +18,6 @@ ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
 CONSUMER_KEY = os.environ['CONSUMER_KEY']
 CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
 
-
 def send_sns_sms(phone_number, message):
     sns = boto3.client('sns')
     print sns.publish(PhoneNumber = phone_number, Message = message)
@@ -35,21 +34,22 @@ def twitter_post(quote):
 def lambda_handler(event, context):
     s3 = boto3.resource('s3')
     s3.Bucket(BUCKET_NAME).download_file("quotes.txt", "/tmp/quotes.txt")
+
     quotes_file = "/tmp/quotes.txt"
 
-    with io.open(quotes_file, "r", encoding = "utf-8") as f:
-        quotes = f.read()
-        quote_list = quotes.splitlines()
-        print quote_list
+    with io.open(quotes_file, "r", encoding = "utf-8") as rf:
+        quotes = rf.read()
 
-        if quote_list: # If quote_list contains items
-            quote = quote_list.pop(random.randrange(len(quote_list))).capitalize()
-            twitter_post(quote)
-        else:
-            message = "The quote well's run dry!"
-            send_sns_sms(PHONE_NUMBER, message)
+    quote_list = filter(None, quotes.splitlines())
 
-        with io.open(quotes_file, "w", encoding = "utf-8") as f:
-            f.write("\n".join(quote_list).decode("utf-8"))
+    if quote_list: # If quote_list contains items
+        quote = quote_list.pop(random.randrange(len(quote_list))).capitalize()
+        twitter_post(quote)
+    else:
+        message = "The quote well's run dry!"
+        send_sns_sms(PHONE_NUMBER, message)
 
-        s3.meta.client.upload_file(quotes_file, BUCKET_NAME, "quotes.txt")
+    with io.open(quotes_file, "w", encoding = "utf-8") as wf:
+        wf.write("\n".join(quote_list).decode("utf-8"))
+
+    s3.meta.client.upload_file(quotes_file, BUCKET_NAME, "quotes.txt")
