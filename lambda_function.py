2b33673b6f0bf9ad02cf3434be47cb9c3fc5975b
cargiggles <20098785+cargiggles@users.noreply.github.com>
2018-11-10 17:10:53 -0800

    v2.0 - Python 3

--- lambda_function.py
+++ lambda_function.py
@@ -1,10 +1,9 @@
 # Andrew Cargill
 # Game Night Tweeter
-# 2018-05-26 - v1.3
+# 2018-11-10 - v2.0 - Conversion From Python 2 to 3
 
-import boto3
+import boto3 
 import os
-import io
 import random
 import twitter
 
@@ -20,7 +19,7 @@ CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
 
 def send_sns_sms(phone_number, message):
     sns = boto3.client('sns')
-    print sns.publish(PhoneNumber = phone_number, Message = message)
+    print(sns.publish(PhoneNumber = phone_number, Message = message))
 
 def twitter_post(quote):
     api = twitter.Api(consumer_key = CONSUMER_KEY,
@@ -29,7 +28,7 @@ def twitter_post(quote):
     access_token_secret = ACCESS_TOKEN_SECRET)
 
     status = api.PostUpdate(quote)
-    print(status.text)
+    print((status.text))
 
 def lambda_handler(event, context):
     s3 = boto3.resource('s3')
@@ -37,10 +36,10 @@ def lambda_handler(event, context):
 
     quotes_file = "/tmp/quotes.txt"
 
-    with io.open(quotes_file, "r", encoding = "utf-8") as rf:
+    with open(quotes_file, "r", encoding = "utf-8") as rf:
         quotes = rf.read()
 
-    quote_list = filter(None, quotes.splitlines())
+    quote_list = [_f for _f in quotes.splitlines() if _f]
 
     if quote_list: # If quote_list contains items
         quote = quote_list.pop(random.randrange(len(quote_list))).capitalize()
@@ -49,7 +48,7 @@ def lambda_handler(event, context):
         message = "The quote well's run dry!"
         send_sns_sms(PHONE_NUMBER, message)
 
-    with io.open(quotes_file, "w", encoding = "utf-8") as wf:
-        wf.write("\n".join(quote_list).decode("utf-8"))
+    with open(quotes_file, "w", encoding = "utf-8") as wf:
+        wf.write("\n".join(quote_list))
 
     s3.meta.client.upload_file(quotes_file, BUCKET_NAME, "quotes.txt")
