a0808e5a0e7641174c6de8a4289c7e1333305447
cargiggles <20098785+cargiggles@users.noreply.github.com>
2021-06-02 18:20:06 -0700

    v2.1
    Migrating From SNS to Twilio To Send SMS Messages

--- lambda_function.py
+++ lambda_function.py
@@ -1,15 +1,16 @@
 # Andrew Cargill
 # Game Night Tweeter
-# 2018-11-10 - v2.0 - Conversion From Python 2 to 3
+# 2021-06-01 - v2.1 - Migrating From SNS to Twilio To Send SMS Messages
 
+import base64
 import boto3 
 import os
 import random
+import requests
 import twitter
 
 # AWS Constants
 BUCKET_NAME = os.environ['BUCKET_NAME']
-PHONE_NUMBER = os.environ['PHONE_NUMBER']
 
 # Twitter Constants
 ACCESS_KEY_TOKEN = os.environ['ACCESS_KEY_TOKEN']
@@ -17,9 +18,29 @@ ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
 CONSUMER_KEY = os.environ['CONSUMER_KEY']
 CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
 
-def send_sns_sms(phone_number, message):
-    sns = boto3.client('sns')
-    print(sns.publish(PhoneNumber = phone_number, Message = message))
+# Twilio Constants
+TWILIO_ACCOUNT_SID = os.environ['TWILIO_ACCOUNT_SID']
+TWILIO_AUTH_TOKEN = os.environ['TWILIO_AUTH_TOKEN']
+TWILIO_PHONE_NUMBER = os.environ['TWILIO_PHONE_NUMBER']
+MY_PHONE_NUMBER = os.environ['MY_PHONE_NUMBER']
+
+def get_auth_header():
+   auth_string = TWILIO_ACCOUNT_SID + ":" + TWILIO_AUTH_TOKEN
+   auth_string_bytes = auth_string.encode("ascii")
+   auth_base_64_bytes = base64.b64encode(auth_string_bytes)
+   auth_base_64_string = auth_base_64_bytes.decode("ascii")
+
+   return "Basic " + auth_base_64_string
+
+def send_twilio_sms(message):
+   url = "https://api.twilio.com/2010-04-01/Accounts/" + TWILIO_ACCOUNT_SID + "/Messages"
+   payload="To=" + MY_PHONE_NUMBER + "&From=" + TWILIO_PHONE_NUMBER + "&Body=" + message
+   headers = {
+      "Authorization": get_auth_header(),
+      "Content-Type": "application/x-www-form-urlencoded"
+   }
+   response = requests.request("POST", url, headers = headers, data = payload)
+   print(response.text)
 
 def twitter_post(quote):
     api = twitter.Api(consumer_key = CONSUMER_KEY,
@@ -46,7 +67,7 @@ def lambda_handler(event, context):
         twitter_post(quote)
     else:
         message = "The quote well's run dry!"
-        send_sns_sms(PHONE_NUMBER, message)
+        send_twilio_sms(message)
 
     with open(quotes_file, "w", encoding = "utf-8") as wf:
         wf.write("\n".join(quote_list))
